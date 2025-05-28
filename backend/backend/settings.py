56a1fe2b6086e097395a610d19841ec718bccc31
Anshulkavi <a234nshul@gmail.com>
2025-05-28 20:21:03 +0530

    update settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -138,6 +138,30 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# For compatibility with credentials and preflight requests
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
 CORS_ALLOW_ALL_ORIGINS = True
 
 
