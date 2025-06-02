21bf12a156546d676d1b6bb58d0d32481de5048d
Anshulkavi <a234nshul@gmail.com>
2025-06-02 22:43:54 +0530

    Update utils.py with latest changes

--- backend/core/utils.py
+++ backend/core/utils.py
@@ -151,20 +151,6 @@ def extract_name(text):
 #     print("[DEBUG] Extracted skills:", found_skills)
 #     return found_skills
 
-def extract_text_from_file(file_path):
-    if file_path.endswith('.pdf'):
-        text = ""
-        with pdfplumber.open(file_path) as pdf:
-            for page in pdf.pages:
-                text += page.extract_text() or ''
-        return text
-    elif file_path.endswith('.docx'):
-        doc = Document(file_path)
-        text = "\n".join(para.text for para in doc.paragraphs)
-        return text
-    else:
-        raise ValueError("Unsupported file format. Use .pdf or .docx")
-
 def extract_skills(text):
     text = text.lower()
 
