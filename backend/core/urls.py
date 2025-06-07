3d4ad20f217db010d398b887b4d64a456455c4fb
Anshulkavi <a234nshul@gmail.com>
2025-06-07 16:48:07 +0530

    checking health  with latest changes

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('upload_resume/', upload_resume, name='upload_resume'),
     path('health_check/', views.health_check, name='health_check'),
-    path('task_status/<str:task_id>/', views.task_status, name='task_status'),
 ]
