1562209389d9c0190483e56b5781eff592e2a29e
Anshulkavi <a234nshul@gmail.com>
2025-06-07 16:55:02 +0530

    Update requirements.txt with new dependencies

--- backend/backend/__init__.py
+++ backend/backend/__init__.py
@@ -1,6 +1,11 @@
+from django.apps import AppConfig
 
-from .celery import app as celery_app
 
-__all__ = ['celery_app']
+class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'core'
+
+    def ready(self):
+        import core.mongo_settings
 
 
