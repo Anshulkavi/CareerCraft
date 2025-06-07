b233e0ae685537ad45fb70ada88edd9fcabf37bc
Anshulkavi <a234nshul@gmail.com>
2025-06-07 12:54:01 +0530

    Update __init__.py with latest changes

--- backend/backend/__init__.py
+++ backend/backend/__init__.py
@@ -3,11 +3,4 @@ from .celery import app as celery_app
 
 __all__ = ['celery_app']
 
-class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
-
-    def ready(self):
-        import core.mongo_settings
-
 
